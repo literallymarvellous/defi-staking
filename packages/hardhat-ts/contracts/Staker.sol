70b94b674f23ef92afa3296387b557e0c6a7fabb
literallymarvellous <ahiaraikechukwu@gmail.com>
2022-07-26 13:03:43 +0100

    edited staking contract

--- packages/hardhat-ts/contracts/Staker.sol
+++ packages/hardhat-ts/contracts/Staker.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import './ExampleExternalContract.sol';
 
-/// @title A staking contract
-/// @author The name of the author
-/// @notice A contract that allows you yo stakie ETH
+/**
+ * @title A staking contract
+ * @author The name of the author
+ *s @notice A contract that allows you yo stakie ETH
+ */
 contract Staker {
   // Events
   event Stake(address sender, uint256 amount);
@@ -13,51 +15,62 @@ contract Staker {
   ExampleExternalContract public exampleExternalContract;
 
   uint256 public constant threshold = 1 ether;
-  uint256 public deadline = block.timestamp + 30 seconds;
-
-  bool openForWithdraw;
-
+  uint256 public deadline = block.timestamp + 3 minutes;
   mapping(address => uint256) public balances;
+
+  /**
+   * @notice Modifier that require the external contract to not be completed
+   */
+  modifier stakeNotCompleted() {
+    bool completed = exampleExternalContract.completed();
+    require(!completed, 'staking process already completed');
+    _;
+  }
+
+  /**
+   * @notice Modifier that require the deadline to be reached or not
+   * @param required Check if the deadlin ehas reached or not
+   */
+  modifier deadlineReached(bool required) {
+    uint256 timeRemaining = timeLeft();
+    if (required) {
+      require(timeRemaining == 0, 'Deadline is not reached yet');
+    } else {
+      require(timeRemaining > 0, 'Deadline is already reached');
+    }
+    _;
+  }
 
   constructor(address exampleExternalContractAddress) public {
     exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
-  // TODO: Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  function stake() public payable {
+  receive() external payable {
+    stake();
+  }
+
+  function stake() public payable deadlineReached(false) stakeNotCompleted {
     balances[msg.sender] += msg.value;
 
     emit Stake(msg.sender, msg.value);
   }
 
-  // TODO: After some `deadline` allow anyone to call an `execute()` function
-  //  It should call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
-  function execute() public {
-    require(block.timestamp >= deadline, "deadline hasn't expired");
-
+  function execute() public deadlineReached(true) stakeNotCompleted {
     uint256 balance_ = address(this).balance;
 
-    if (balance_ > threshold) {
-      exampleExternalContract.complete{value: balance_}();
-    } else {
-      openForWithdraw = true;
-    }
+    require(balance_ >= threshold, "balance isn't over threshlod");
+
+    exampleExternalContract.complete{value: balance_}();
+    require(exampleExternalContract.completed(), 'execute failed');
   }
 
-  // TODO: if the `threshold` was not met, allow everyone to call a `withdraw()` function
-  function withdraw() public {
-    require(block.timestamp >= deadline, "deadline hasn't expired");
-    require(openForWithdraw, 'Not open for withdrawal');
-
+  function withdraw() public deadlineReached(true) stakeNotCompleted {
     uint256 balance_ = balances[msg.sender];
 
-    // check if the user has balance to withdraw
     require(balance_ > 0, 'No balance to withdraw');
 
-    // reset the balance of the user
     balances[msg.sender] = 0;
 
-    // Transfer balance back to the user
     (bool sent, ) = msg.sender.call{value: balance_}('');
     require(sent, 'Failed to withdraw');
   }
@@ -68,9 +81,5 @@ contract Staker {
     } else {
       return deadline - block.timestamp;
     }
-  }
-
-  receive() external payable {
-    stake();
   }
 }
